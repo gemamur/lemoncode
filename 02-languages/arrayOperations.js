0214abde4327514374a98c752298ce892c92b84f
Gema_Munoz <g.munoz@enchufesolar.com>
2022-10-07 13:06:04 +0200

    corrección arrow functions

--- 02-languages/arrayOperations.js
+++ 02-languages/arrayOperations.js
@@ -1,15 +1,13 @@
 // el primero (destructuring)
-const head = (arr) => arr.at(0);// Implementation here.
+const head = ([a]) => a;
+// Implementation here.
 
 console.log(head([24,1,2,3,4]));//devuelve 24
 
 
 // todos menos el primero (rest)
 
-const tail = (arr) => {
-    const [first, ...arr2] = arr;
-    return arr2;
-}
+const tail = ([, ...arr2]) => arr2;
 console.log(tail([24,1,2,3,4]));//devuelve [1,2,3,4]
 
 
