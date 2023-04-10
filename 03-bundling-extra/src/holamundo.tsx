9c0a307d1017735943b5fb94695b12fbc0dd0941
gemamur <33229517+gemamur@users.noreply.github.com>
2023-04-10 19:20:22 +0200

    update

--- 03-bundling-extra/src/holamundo.tsx
+++ 03-bundling-extra/src/holamundo.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 export const HolaMundo = () => {
+    const url = process.env.API_BASE_URL;
+    console.log(url);
     return(
         <>
             <div>Hola Mundo!</div>
